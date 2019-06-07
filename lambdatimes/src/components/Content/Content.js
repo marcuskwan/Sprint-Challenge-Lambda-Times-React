1b3f884d20b34731f6528442238388a61bbbbce2
Marcus Kwan <kwan.marcus@gmail.com>
2019-06-08 02:09:04 +1000

    finished cards, need to add proptypes in tabs

--- lambdatimes/src/components/Content/Content.js
+++ lambdatimes/src/components/Content/Content.js
@@ -41,7 +41,7 @@ export default class Content extends Component {
           of the items from cardData. 
         - else, it should only return those cards whose 'tab' matched this.state.selected.
     */
-    // return this.state.cards;
+    return this.state.cards;
   };
 
   render() {
