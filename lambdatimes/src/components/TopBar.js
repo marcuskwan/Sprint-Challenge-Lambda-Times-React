ff72f1b769e8bf4de63108758fddedcc0f4567aa
Marcus Kwan <kwan.marcus@gmail.com>
2019-06-08 03:41:31 +1000

    finished topbarstyledcomponents

--- lambdatimes/src/components/TopBar.js
+++ lambdatimes/src/components/TopBar.js
@@ -1,5 +1,11 @@
 import React from "react";
-import { TopBarDiv } from "../CSS/TopBarStyledComponents";
+import {
+  TopBarDiv,
+  Container,
+  ContainerLeft,
+  ContainerCenter,
+  ContainerRight,
+} from "../CSS/TopBarStyledComponents";
 
 // Refactor this component to use styled components and not classNames.
 // You can find the corresponding CSS in the CSS/index.css file
@@ -7,22 +13,22 @@ import { TopBarDiv } from "../CSS/TopBarStyledComponents";
 const TopBar = () => {
   return (
     <TopBarDiv>
-      <div className="container">
-        <div className="container-left">
+      <Container>
+        <ContainerLeft>
           <span>TOPICS</span>
           <span>SEARCH</span>
-        </div>
-        <div className="container-center">
+        </ContainerLeft>
+        <ContainerCenter>
           <span>GENERAL</span>
           <span>BROWNBAG</span>
           <span>RANDOM</span>
           <span>MUSIC</span>
           <span>ANNOUNCEMENTS</span>
-        </div>
-        <div className="container-right">
+        </ContainerCenter>
+        <ContainerRight>
           <span>LOG IN</span>
-        </div>
-      </div>
+        </ContainerRight>
+        </Container>
     </TopBarDiv>
   );
 };
